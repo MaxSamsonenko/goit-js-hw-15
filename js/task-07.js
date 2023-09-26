c4107e950afea8023d4d85196dc518af67c9d932
Max Samsonenko <samsonenko.maxwork@gmail.com>
2023-09-26 20:35:55 +0300

    task 7 fix

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,7 @@
 const sizeCtrl = document.querySelector("#font-size-control");
 
 const text = document.querySelector("#text");
+text.style.fontSize = `${sizeCtrl.value}px`;
 
 const handleCtrl = (evt) => {
 	text.style.fontSize = `${evt.currentTarget.value}px`;
